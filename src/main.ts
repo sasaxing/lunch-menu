6c09ba17655e6471647f8e1d2403b5e033475a77
Xiaosha Xing <xiaosha.xing@goto.com>
2022-09-26 21:43:35 +0200

    feat: initial files

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./assets/main.css";
+
+// create App
+const app = createApp(App);
+
+// configure App
+app.config.errorHandler = function (err) {
+  console.error(" aaaaaa ", err);
+};
+
+// app.component("testComponent", testComponent);
+
+// mount
+app.mount("#app");
