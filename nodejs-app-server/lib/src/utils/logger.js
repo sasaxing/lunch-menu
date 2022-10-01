b677caed3ebc363dbf62b61c0194ac64559b3c3c
sasaxing <xiaosha_xing@outlook.com>
2022-10-01 22:18:30 +0200

    build: ignore all build files

--- nodejs-app-server/lib/src/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-function log(message, arg) {
-    console.log('[server]' + message, arg);
-}
-exports.log = log;
