3d50a73501114cad0efea57ce3bc35ebc55ea820
sasaxing <xing.theresa@gmail.com>
2022-10-15 22:01:44 +0200

    refactor(node): exit process gracefully on SIGTERM and SIGINT

--- nodejs-app-server/src/index.ts
+++ nodejs-app-server/src/index.ts
@@ -9,6 +9,27 @@ const defaultMongoDbConfig = {
 
 const appServer = new AppServer(defaultMongoDbConfig);
 appServer.setup(3000).then((result) => {
+    log('Server started', {result})
 }).catch((reason) => {
     log('Error happens during setup!', {reason});
 });
+
+process.addListener('uncaughtException', (what) => {
+    log('uncaughtException', { what })
+});
+
+process.addListener('unhandledRejection', (what) => {
+    log('unhandledRejection', { what })
+});
+
+// https://nodejs.org/api/process.html#signal-events
+process.on('SIGINT', (signal) => {
+    log('Process was interrupted by ctrl+c: ', { signal });
+    process.exit();
+});
+
+process.on('SIGTERM', (signal) => {
+    log('Process was terminated, possibly by kill', { signal });
+    process.exit();
+});
+
